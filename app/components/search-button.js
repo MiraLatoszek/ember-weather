122cf9c0b4349f17239ce4e0c916f6db4435ca4d
Dave Laird <kiwiupover@gmail.com>
2014-01-17 19:25:10 -0800

    WIP

--- app/components/search-button.js
+++ app/components/search-button.js
@@ -3,10 +3,21 @@ import getJSON from "appkit/utils/get-json";
 export default Ember.Component.extend({
   classNames: ['row', 'search'],
   searchTerm: null,
-  results: [],
+  name: 'name',
+  results: ['dave'],
 
-  triggerSearch: function () {
-    this.sendAction("searchMessage", this.get('searchTerm'));
+  triggerSearch: function (data) {
+    var _this = this;
+    this.$('input').typeahead({
+      name: "typeahead",
+      local: data.map(function(item) {
+        return {
+          value: item.name,
+          name: item.name,
+          emberObject: item
+        };
+      })
+    });
   }.on('didInsertElement'),
 
   triggerAutoComplete: function () {
@@ -16,22 +27,67 @@ export default Ember.Component.extend({
         self.set('results', self._parseResults(data));
       });
     }, 400);
+
   }.observes('searchTerm'),
 
-  openResults: function() {
-    if (this.get('results').length > 1) {
+  // triggerTypeahead: function() {
+  //   var _this = this,
+  //       r = [],
+  //       data = this.get('results');
 
-      var input = this.$().find('input'),
-          inputOffsetTop = input.offset().top,
-          inputOffsetLeft = input.offset().left,
-          inputHeight = input.outerHeight(),
-          topOffset = inputOffsetTop + inputHeight,
+  //   r =  data.map(function(item) {
+  //     return {
+  //       value: item.name,
+  //       name: item.name,
+  //       emberObject: item
+  //     };
+  //   });
+  //   window.console.log('results %o', r);
+  // }.observes('results'),
 
-          resultDropdown = this.$().find('#result-dropdown');
+  initializeTypeahead: function(data){
+    var _this = this;
+    this.typeahead = this.$().typeahead({
+      name: "typeahead",
+      limit: this.get("limit") || 5,
+      local: data.map(function(item) {
+        return {
+          value: item.get("name"),
+          name: item.get("name"),
+          tokens: [item.get("name")],
+          emberObject: item
+        };
+      })
+    });
 
-      return resultDropdown.css({'top': topOffset, 'left': inputOffsetLeft});
+    this.typeahead.on("typeahead:selected", function(event, item) {
+      _this.set("selection", item.emberObject);
+    });
+
+    this.typeahead.on("typeahead:autocompleted", function(event, item) {
+      _this.set("selection", item.emberObject);
+    });
+
+    if (this.get("selection")) {
+      this.typeahead.val(this.get("selection.name"));
     }
   }.observes('results'),
+
+
+  // openResults: function() {
+  //   if (this.get('results').length > 1) {
+
+  //     var input = this.$().find('input'),
+  //         inputOffsetTop = input.offset().top,
+  //         inputOffsetLeft = input.offset().left,
+  //         inputHeight = input.outerHeight(),
+  //         topOffset = inputOffsetTop + inputHeight,
+
+  //         resultDropdown = this.$().find('#result-dropdown');
+
+  //     return resultDropdown.css({'top': topOffset, 'left': inputOffsetLeft});
+  //   }
+  // }.observes('results'),
 
   closeResultDropdown: function(){
     this.$('#result-dropdown').css({'left': '-999999px'});
@@ -47,6 +103,6 @@ export default Ember.Component.extend({
 
   _parseResults: function(data){
     var parsedData = JSON.parse(data[1]);
-    return parsedData.RESULTS;
+    return parsedData.RESULTS.filterProperty('type', 'city');
   }
 });
